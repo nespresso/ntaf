418325eb766cca5bd7da8063194250706abddefa
David RACODON <david.racodon@gmail.com>
2017-05-24 18:33:16 +0200

    Add SonarQube to Travis CI

---
+++ travis.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_NODE_VERSION" == "4" ]; then
+  node --harmony_rest_parameters ./node_modules/.bin/grunt test
+  node --harmony_rest_parameters ./node_modules/.bin/grunt test-with-coverage
+else
+  node ./node_modules/.bin/grunt test
+  node ./node_modules/.bin/grunt test-with-coverage
+fi
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_NODE_VERSION" == "4" ]; then
+  sonar-scanner -Dsonar.host.url=https://sonarqube.com \
+                -Dsonar.analysis.mode=preview \
+                -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+                -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+                -Dsonar.github.oauth=$GITHUB_TOKEN \
+                -Dsonar.login=$SONARQUBE_TOKEN
+
+elif [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && ["$TRAVIS_NODE_VERSION" == "4"]; then
+  sonar-scanner -Dsonar.host.url=https://sonarqube.com -Dsonar.login=$SONARQUBE_TOKEN
+fi
