cb0f880e6c16d00a183a740f8aebd78d8cbbd229
David RACODON <david.racodon@gmail.com>
2017-05-29 13:31:53 +0200

    Fix #17 Generate JSDoc

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,12 +24,22 @@ module.exports = function (grunt) {
         },
       },
     },
+
+    jsdoc: {
+      dist: {
+        src: ['lib/browser-command/*.js', 'lib/helper/helper.js'],
+        options: {
+          destination: 'docs',
+        },
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-mocha-istanbul');
+  grunt.loadNpmTasks('grunt-jsdoc');
 
   grunt.registerTask('test', 'Run unit tests', 'mochaTest');
   grunt.registerTask('test-with-coverage', 'Run unit tests with code coverage computation', 'mocha_istanbul');
-
+  grunt.registerTask('doc', 'Generate JSDoc', 'jsdoc');
 };
