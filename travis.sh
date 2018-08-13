63de0e75c07afef0127d8c6889a418770be906b5
David RACODON <david.racodon@gmail.com>
2018-08-13 17:59:00 +0200

    Rename mvnCommand into command

--- travis.sh
+++ travis.sh
@@ -9,7 +9,7 @@ npm run test-with-coverage
 npm run doc
 npm run e2e-test-docker
 
-mvnCommand='sonar-scanner'
+command='sonar-scanner'
 commonArgs="-Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN -Dsonar.organization=nespresso"
 githubArgs="-Dsonar.analysis.mode=preview -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST -Dsonar.github.repository=$TRAVIS_REPO_SLUG -Dsonar.github.oauth=$GITHUB_TOKEN"
 branchArgs=''
@@ -18,7 +18,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS_BRANCH" != "master" ]; then
        branchArgs="-Dsonar.branch.name=$TRAVIS_BRANCH -Dsonar.branch.target=master"
     fi
-    eval $mvnCommand $commonArgs $branchArgs
+    eval $command $commonArgs $branchArgs
 else
-    eval $mvnCommand $commonArgs $githubArgs
+    eval $command $commonArgs $githubArgs
 fi
